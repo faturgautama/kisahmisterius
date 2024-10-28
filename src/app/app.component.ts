2be98af345a82f9dc6964357aa3f50411351c962
Fatur Gautama <fatur@paycombat.com>
2024-10-28 09:00:42 +0700

    feat: init deps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterOutlet } from '@angular/router';
+import * as AOS from 'aos';
+import { ToastModule } from 'primeng/toast';
+import { ProgressBarModule } from 'primeng/progressbar';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [
+        RouterOutlet,
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        ToastModule,
+        ProgressBarModule
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'kisahmisterius';
+export class AppComponent implements OnInit, OnDestroy {
+
+    title = 'kisahmisterius';
+
+    constructor() { }
+
+    ngOnInit(): void {
+        AOS.init();
+    }
+
+    ngOnDestroy(): void {
+
+    }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,13 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { MessageService } from 'primeng/api';
+import { CookieService } from 'ngx-cookie-service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+    providers: [
+        provideRouter(routes),
+        MessageService,
+        CookieService,
+    ]
 };

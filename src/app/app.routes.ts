c52922cc893c64e8a88a3d018a70eda4654c8a01
Fatur Gautama <fatur@paycombat.com>
2024-11-17 21:28:40 +0700

    feat: change routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,8 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: async () => (await import('./pages/beranda/beranda.component')).BerandaComponent
+        // loadComponent: async () => (await import('./pages/beranda/beranda.component')).BerandaComponent
+        loadChildren: async () => (await import('./pages/admin/admin.routes')).adminRoutes
     },
     {
         path: 'admin',
